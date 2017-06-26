9b93e17c34c38c1b9f9e0c9c4f1b9f763067d08d
adhv45 <chhipaanil98@gmail.com>
2017-06-26 18:57:45 +0530

    new

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,41 +1,77 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {SecondPage} from "../second/second";
+import {DatafetchProvider} from "../../providers/datafetch/datafetch";
+import {Http, RequestOptions, Headers} from "@angular/http";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-username:string;
-password:string;
-addcomments:string;
-comment:any=["anil","arpan","jayash"];
-student_name:string;
-student_batch:number;
-student_year:string;
-students:any=[{"name":"xyz","batch":"2015","year":"second"},{"name":"abc","batch":"2016","year":"first"},
-              {"name":"pqr","batch":"2014","year":"third"}];
-  constructor(public navCtrl: NavController) {
+  username: string;
+  password: string;
+  addcomments: string;
+  pass:string;
+  comment: any = ["anil", "arpan", "jayash"];
+  student_name: string;
+  student_batch: number;
+  student_year: string;
+  students: any;
+  all: any;
+  update: any;
 
+  constructor(public navCtrl: NavController, public datafetch: DatafetchProvider, public http: Http) {
+
+    this.getdata();
   }
 
-  addcomment()
-  {
+
+  getdata() {
+    this.datafetch.load().then((data) => {
+
+      this.students = (data);
+      console.log(JSON.stringify(data));
+      this.all = this.students.students;
+    })
+  }
+
+  addcomment() {
     this.comment.push(this.addcomments);
-    this.addcomments="";
+    this.addcomments = "";
   }
-  removeItem(){
+
+  removeItem() {
     this.comment.pop(this.addcomments);
-    }
-  addstudent(){
-    this.students.push({"name":this.student_name,"batch":this.student_batch,"year":this.student_year});
   }
 
-  gotosecondpage()
-  {
-    if(this.username=="anil"&&this.password=="234598")
-    this.navCtrl.push(SecondPage,{"username":this.username});
+  //addstudent(){
+  //this.students.push({"name":this.student_name,"batch":this.student_batch,"year":this.student_year});
+  //}
+
+  gotosecondpage() {
+    //if (this.username == "anil" && this.password == "234598")
+      this.navCtrl.push(SecondPage, {"username": this.username});
   }
 
+  setdata() {
+    this.update = {
+      name: this.username,
+      password: this.password,
+    };
+    console.log("data sending");
+    var headers = new Headers();
+
+    headers.append('content-type','application/json;charset=UTF-8');
+    headers.append('Access-Control-Allow-Origin','*');
+    let options = new RequestOptions({headers:headers});
+
+    this.http.post("https://vehiclevaioti.herokuapp.com/insert",JSON.stringify(this.update),options)
+      .map(res => res.json()).subscribe(data=>{
+      console.log(data)
+    }, err =>{
+      console.log("Error!:",err.json());
+    });
+    this.gotosecondpage();
+  }
 }
